1b4882eac3955b2775af03af0e9c8cec32870427
mohiitt <mohitbarade1@gmail.com>
2024-07-07 17:13:33 +0530

    signup elements added

--- frontend/BTS/src/Home/Register.jsx
+++ frontend/BTS/src/Home/Register.jsx
@@ -1,60 +1,62 @@
-import './index.scss'
-import vid from "../Assets/xyz.gif"
-import 'boxicons'
-import { FcGoogle } from "react-icons/fc";
+import "./index.scss";
+import vid from "../Assets/xyz.gif";
+import "boxicons";
+
 import { useNavigate } from "react-router-dom";
 
 export default function Register() {
-
-    let navigate= useNavigate()
-
+  let navigate = useNavigate();
 
   return (
-    <div className='container'>
-    <div className='box'>
-        <div className='welcome'>Welcome to BTS!</div>
-        <div className='gif'><img src={vid} alt="loading..." /></div>
-
-        <div className='login'>
-          <form>
-            <div className='input'>
-              <input type='text' placeholder='Email' required /><box-icon name='user-circle' type='solid' color='#71c563' ></box-icon>
-            </div>
-            <div className='input'>
-              <input type='text' placeholder='Password' required /><box-icon type='solid' name='lock-alt' color='#71c563'></box-icon>
-            </div>
-            <div className='input'>
-              <button type='submit' className='btn'>SIGN UP</button>
-            </div>
-
-
-            <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-            <div style={{flex: 1, height: '1px', backgroundColor: 'GREY'}} />
-
-            <div style={{padding: '0 2px'}}>
-            <p style={{width: '70px', textAlign: 'center',fontWeight: 'bold',color:'black'}}>OR</p>
-            </div>
-            <div style={{flex: 1, height: '1px', backgroundColor: 'GREY'}} /></div>
-
-            <div className='google'>
-            <button className='g-btn' onClick={() => { console.log('Google sign-in button clicked') }}>
-            <FcGoogle size={25} />
-            </button>
-            </div>
-
-            <div className='sign-up'>
-                <p>Already have an account?<br></br><a className="link" onClick={() => navigate("/")}>SignIn</a></p>
-                
-            </div>
-
-          </form>
-
-          
-
+    <div className="container">
+      <div className="box">
+        <div className="welcome">Welcome to BTS!</div>
+        <div className="gif">
+          <img src={vid} alt="loading..." />
         </div>
 
+        <div className="login">
+          <form>
+            <div className="input">
+              <input type="text" placeholder="Full Name" required />
+              <box-icon
+                name="user-circle"
+                type="solid"
+                color="#71c563"
+              ></box-icon>
+            </div>
+            <div className="input">
+              <input type="text" placeholder="Email" required />
+              <box-icon
+                name="user-circle"
+                type="solid"
+                color="#71c563"
+              ></box-icon>
+            </div>
+            <div className="input">
+              <input type="text" placeholder="Password" required />
+              <box-icon type="solid" name="lock-alt" color="#71c563"></box-icon>
+            </div>
+            <div className="input">
+              <input type="text" placeholder="Confirm Password" required />
+              <box-icon type="solid" name="lock-alt" color="#71c563"></box-icon>
+            </div>
+            <div className="input">
+              <button type="submit" className="btn">
+                SIGN UP
+              </button>
+            </div>
+            <div className="sign-up">
+              <p>
+                Already have an account?<br></br>
+                <a className="link" onClick={() => navigate("/")}>
+                  SignIn
+                </a>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
-    </div>
- 
-  )
+  );
 }
