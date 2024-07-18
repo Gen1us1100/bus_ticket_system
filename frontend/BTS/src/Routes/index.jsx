2b23dd1e6c26ff71c84cfbf105e75a2df215a12f
mohiitt <mohitbarade1@gmail.com>
2024-07-18 11:23:13 +0530

    a

--- frontend/BTS/src/Routes/index.jsx
+++ frontend/BTS/src/Routes/index.jsx
@@ -1,19 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
-import Login from "../Home/SignIn";
-import Register from "../Home/Register";
+import Login from "../Login/SignIn";
+import Register from "../Login/Register";
 import Navbar from "../Navbar/navbar";
 
 export const router = createBrowserRouter([
-    {
-        path:"/",
-        element:<Login />
-    },
-    {
-        path:"/register",
-        element:<Register />
-    },
-    {
-        path:"/nav",
-        element:<Navbar />
-    },
-])
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/nav",
+    element: <Navbar />,
+  },
+]);
