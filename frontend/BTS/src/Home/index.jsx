79ffa0941975e295ef5f332a8310c39288f35302
PIYUSH <piyush.class@gmail.com>
2024-07-30 09:28:54 +0530

    assd

--- frontend/BTS/src/Home/index.jsx
+++ frontend/BTS/src/Home/index.jsx
@@ -45,60 +45,67 @@ export default function Home() {
     <div className="text1"><FaBus />pmpml
       <h1>hey user</h1>
       <div className="homeBox">
-        <h1>From</h1>
+        <h1 className="from-to">From</h1>
         <div className="search-bar-container">
           <div className="searchbar">
-            <input type="text" value={fromValue} onChange={onFromChange} placeholder="from"/>
+            <input type="text" value={fromValue} onChange={onFromChange} placeholder="from" />
+            {fromValue && (
+              <div className={`dropdown ${fromValue ? 'show' : ''}`}>
+                {uniqFrom.filter(fromLocation => {
+                  const searchTerm = fromValue.toLowerCase();
+                  const from = fromLocation.toLowerCase();
+
+                  return searchTerm && from.startsWith(searchTerm) && from !== searchTerm;
+                }).slice(0, 10)
+                  .map((fromLocation, index) => (
+                    <div
+                      onClick={() => onFromSearch(fromLocation)}
+                      className="dropdown-row"
+                      key={`${fromLocation}-${index}`}
+                    >{fromLocation}</div>
+                  ))}
+              </div>
+            )}
           </div>
         </div>
-        <div className="dropdown">
-          {uniqFrom.filter(fromLocation => {
-            const searchTerm = fromValue.toLowerCase();
-            const from = fromLocation.toLowerCase();
 
-            return searchTerm && from.startsWith(searchTerm) && from !== searchTerm;
-          }).slice(0, 10)
-            .map((fromLocation, index) => (
-              <div
-                onClick={() => onFromSearch(fromLocation)}
-                className="dropdown-row"
-                key={`${fromLocation}-${index}`}
-              >{fromLocation}</div>
-            ))}
-        </div>
-
-        <h1>To</h1>
+        <h1 className="from-to">To</h1>
         <div className="search-bar-container">
           <div className="searchbar">
-            <input type="text" value={toValue} onChange={onToChange} />
+            <input type="text" value={toValue} onChange={onToChange} placeholder="to" />
+            {toValue && (
+              <div className={`dropdown ${toValue ? 'show' : ''}`}>
+                {uniqTo.filter(toLocation => {
+                  const searchTerm = toValue.toLowerCase();
+                  const to = toLocation.toLowerCase();
+
+                  return searchTerm && to.startsWith(searchTerm) && to !== searchTerm;
+                }).slice(0, 10)
+                  .map((toLocation, index) => (
+                    <div
+                      onClick={() => onToSearch(toLocation)}
+                      className="dropdown-row"
+                      key={`${toLocation}-${index}`}
+                    >{toLocation}</div>
+                  ))}
+              </div>
+            )}
           </div>
         </div>
-        <div className="dropdown">
-          {uniqTo.filter(toLocation => {
-            const searchTerm = toValue.toLowerCase();
-            const to = toLocation.toLowerCase();
+        
+        <h1 className="from-to" >Date</h1>
+        <input className="date" type="date"></input>
 
-            return searchTerm && to.startsWith(searchTerm) && to !== searchTerm;
-          }).slice(0, 10)
-            .map((toLocation, index) => (
-              <div
-                onClick={() => onToSearch(toLocation)}
-                className="dropdown-row"
-                key={`${toLocation}-${index}`}
-              >{toLocation}</div>
-            ))}
-        </div>
 
-        <h1>Price</h1>
+        <h1 className="from-to" > Price</h1>
         
         {price && <div className="price-display">{price}</div>}
       </div>
 
       <div className="pass">
-        <button className="pass-btn"><IoTicket size={20} color="#3bab40"/><br />Daily<br /> Pass</button>
-        <button className="pass-btn"><IoTicket size={20} color="#3bab40"/><br />Monthly<br /> Pass</button>
+        <button className="pass-btn"><IoTicket size={20} className="icon4"/><br />Daily<br /> Pass</button>
+        <button className="pass-btn"><IoTicket size={20} className="icon4"/><br />Monthly<br /> Pass</button>
       </div>
-
     </div>
   )
 }
